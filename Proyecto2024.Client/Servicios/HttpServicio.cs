810c02cb6bbc9c42856453733631f3cbdf63e119
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2024-08-19 12:06:41 -0300

    Servicio http en el Cliente

---
+++ Proyecto2024.Client/Servicios/HttpServicio.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+
+namespace Proyecto2024.Client.Servicios
+{
+    public class HttpServicio
+    {
+        private readonly HttpClient http;
+
+        public HttpServicio(HttpClient http)
+        {
+            this.http = http;
+        }
+
+        public async Task<HttpRespuesta<T>> Get<T>(string url) //https://localhost:7268/api/TDocumentos
+        {
+            var response = await http.GetAsync(url);
+
+            if(response.IsSuccessStatusCode)
+            {
+                var respuesta = await DesSerializar<T>(response);
+                return new HttpRespuesta<T>(respuesta, false, response);
+            }
+            else
+            {
+                return new HttpRespuesta<T>(default, true, response);
+            }
+        }
+
+        private async Task<T?> DesSerializar<T>(HttpResponseMessage response)
+        {
+            var respuestaStr = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(respuestaStr,
+                new JsonSerializerOptions(){ PropertyNameCaseInsensitive = true });
+        }
+    }
+}
---
+++ Proyecto2024.Client/Servicios/Httprespuesta.cs
@@ -0,0 +1,19 @@
+﻿namespace Proyecto2024.Client.Servicios
+{
+    public class HttpRespuesta<T>
+    {
+        public T Respuesta { get; }
+
+        public bool Error { get; }
+
+        public HttpResponseMessage HttpResponseMessage { get; set; }
+
+        public HttpRespuesta(T respuesta, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Respuesta = respuesta;
+            Error = error;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+    }
+}
