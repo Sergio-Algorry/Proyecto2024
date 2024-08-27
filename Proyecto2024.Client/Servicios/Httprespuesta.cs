2545d5bee1473d1040cc8ff4bd78c06e968c77d6
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2024-08-26 23:20:48 -0300

    ListaTDocumento

--- Proyecto2024.Client/Servicios/Httprespuesta.cs
+++ Proyecto2024.Client/Servicios/Httprespuesta.cs
@@ -15,5 +15,30 @@
             HttpResponseMessage = httpResponseMessage;
         }
 
+        public async Task<string> ObtenerError()
+        {
+            if (!Error)
+            {
+                return "";
+            }
+
+            var statuscode = HttpResponseMessage.StatusCode;
+
+            switch (statuscode)
+            {
+                case System.Net.HttpStatusCode.BadRequest:
+                    return HttpResponseMessage.Content.ReadAsStringAsync().ToString()!;
+                //                    return "Error, no se puede procesar la información";
+                case System.Net.HttpStatusCode.Unauthorized:
+                    return "Error, no está logueado";
+                case System.Net.HttpStatusCode.Forbidden:
+                    return "Error, no tiene autorización a ejecutar este proceso";
+                case System.Net.HttpStatusCode.NotFound:
+                    return "Error, dirección no encontrado";
+                default:
+                    return HttpResponseMessage.Content.ReadAsStringAsync().Result;
+            }
+        }
+
     }
 }
