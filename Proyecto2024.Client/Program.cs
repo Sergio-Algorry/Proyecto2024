74517f16b378fd48a2c2b40040ffefc128905407
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2024-09-10 18:16:35 -0300

    Put

--- Proyecto2024.Client/Program.cs
+++ Proyecto2024.Client/Program.cs
@@ -8,6 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 builder.Services.AddScoped<IHttpServicio, HttpServicio>();
 
 await builder.Build().RunAsync();
--- Proyecto2024.Client/Servicios/HttpServicio.cs
+++ Proyecto2024.Client/Servicios/HttpServicio.cs
@@ -49,6 +49,26 @@ namespace Proyecto2024.Client.Servicios
             }
         }
 
+        public async Task<HttpRespuesta<object>> Put<T>(string url, T entidad)
+        {
+            var enviarJson = JsonSerializer.Serialize(entidad);
+
+            var enviarContent = new StringContent(enviarJson,
+                                Encoding.UTF8,
+                                "application/json");
+
+            var response = await http.PutAsync(url, enviarContent);
+            if (response.IsSuccessStatusCode)
+            {
+                var respuesta = await DesSerializar<object>(response);
+                return new HttpRespuesta<object>(respuesta, false, response);
+            }
+            else
+            {
+                return new HttpRespuesta<object>(default, true, response);
+            }
+        }
+
         private async Task<T?> DesSerializar<T>(HttpResponseMessage response)
         {
             var respuestaStr = await response.Content.ReadAsStringAsync();
